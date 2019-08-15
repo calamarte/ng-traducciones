45189975bdc1f357b6948ef67f720e46c721f37d
Jose <calamarte@gmail.com>
2019-08-15 21:57:53 +0200

    introducida la libreria de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LyThemeModule, LY_THEME } from '@alyle/ui';
+import { LyButtonModule } from '@alyle/ui/button';
+import { LyIconModule } from '@alyle/ui/icon';
+import { LyTypographyModule } from '@alyle/ui/typography';
+import { MinimaLight, MinimaDark } from '@alyle/ui/themes/minima';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LyThemeModule.setTheme('minima-light'),
+    LyButtonModule,
+    LyIconModule,
+    LyTypographyModule
   ],
-  providers: [],
+  providers: [{ provide: LY_THEME, useClass: MinimaLight, multi: true }, { provide: LY_THEME, useClass: MinimaDark, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
